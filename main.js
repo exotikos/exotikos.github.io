87ecda64ecb96517a61213822150dedb12a4989c
exotikos <93763974+exotikos@users.noreply.github.com>
2023-11-02 14:59:02 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function redirectMath() {
+    window.location.href = "https://exotikos.github.io/math";
+  }
+  function redirectGist() {
+    window.location.href = "https://exotikos.github.io/gists";
+  }
+  function redirectMemes() {
+    window.location.href = "https://exotikos.github.io/memes";
+  }
