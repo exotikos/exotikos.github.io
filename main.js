9d1175108e640836b350990710fe51a4ab96acfb
exotikos <93763974+exotikos@users.noreply.github.com>
2023-11-02 23:48:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ function redirectMath() {
   function redirectMemes() {
     window.location.href = "https://exotikos.github.io/memes";
   }
+  setInterval(function() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth <= 1600) {
+      const element = document.getElementById("gists");
+      const halfScreenWidth = screenWidth / 2.4;
+      element.style.left = halfScreenWidth + "px";
+      element.style.transform = `translateX(${halfScreenWidth})`;
+    } else if (screenWidth >= 1600) {
+    const element = document.getElementById("gists");
+    const halfScreenWidth = screenWidth / 2.2;
+    element.style.left = halfScreenWidth + "px";
+    element.style.transform = `translateX(${halfScreenWidth})`;
+    } else if (screenWidth <= 1100) {
+      const element = document.getElementById("gists");
+      element.left = 0 + "px";
+      }
+}, 2000);
